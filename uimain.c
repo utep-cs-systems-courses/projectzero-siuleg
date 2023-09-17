cc09909bb89d1f96d2b6f6e2cf2b3c718739ef2d
siuleg <jlrodriguez29@miners.utep.edu>
2023-09-17 13:47:32 -0600

    Added 8x12 font

--- uimain.c
+++ uimain.c
@@ -27,7 +27,7 @@ int main()
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c <= 'Z'; c++)
-      	print_char_5x7(c);
+        print_char_8x12(c);
       break;
     case 'a':
       puts("You selected arrow:");
